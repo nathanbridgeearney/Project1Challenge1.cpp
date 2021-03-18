2836b287acb74f95b84fb05d6228dd15ddf76b16
nathanbridgeearney <80077609+nathanbridgeearney@users.noreply.github.com>
2021-03-18 20:02:21 +1300

    first

--- main.cpp
+++ main.cpp
@@ -1,30 +1,53 @@
+#include <iostream> // input-output library
+#include <cmath>  // library for sin function
 #include "wav.hpp" // make sure to include this helper library
+#include <fstream>
 // " " instead of <> means that library is in the same folder as your program
 
 using namespace std;
 
 
-int main(){
+int main() {
     WavSound sound1; // helper
-    double sample_rate = 41000; // samples per second
+    double sample_rate = 41400; // samples per second
     // your code goes here
-    double duration = 5.0;
-    double freq;
-    double dt = duration/sample_rate;
-    int n_samples = sample_rate * duration;
-    int * waveform = new int[n_samples]; // creates the array
+    string freq;
+    ifstream readFile("odetojoy.txt");
+    double dt = 1 / sample_rate;
+    int duration = 15;
+    int n_samples = duration / dt;
+    int *waveform = new int[n_samples]; // creates the array
+    string line; int lineN = 0;
+    while (getline(readFile, line))lineN ++;
+    int w_sample = n_samples/lineN;
+    readFile.close();
+    int* notes = new int[lineN];
+    readFile.open("odetojoy.txt");
+    lineN = 0;
 
+    while (getline(readFile, line)) {
+        // Output the text from the file
+        notes[lineN] = stod(line);
+        cout<<line;
+        lineN++;
+    }
 
-    for ( int i_sample = 0; i_sample < n_samples ; i_sample++) {
-        if ((i_sample / 25625) % 2 == 0) {
-            freq = 800;
-            waveform[i_sample] = 6000 * sin(2 * M_PI * freq * i_sample * dt);
-        } else {
-            freq = 1000;
-            waveform[i_sample] = 5000 * sin(2 * M_PI * freq * i_sample * dt);
+    for (int i = 0; i < lineN; i++) {
+
+        for (int i_sample = 0; i_sample < w_sample; i_sample++) {
+            //and here
+            waveform[i_sample + i * w_sample] = 7000 * sin(2 * M_PI * notes[i] * i_sample * dt);
+           // cout<<"\n making wave " <<  notes[i];
         }
     }
 
-    sound1.MakeWavFromInt("tone1.wav",sample_rate, waveform, n_samples); //file name can be changed but keep extension .wav
+
+
+// Close the file
+    readFile.close();
+
+    sound1.MakeWavFromInt("tone1.wav", sample_rate, waveform, n_samples); //file name can be changed but keep extension .wav
     delete[](waveform);
     return 0;
+
+}
